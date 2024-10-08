6414a7ebefc65ae99ef460709b9f69fe2c997c42
NY <67083864+nycan@users.noreply.github.com>
2024-10-08 13:56:31 -0600

    wrong type of object detection :skull:

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -8,21 +8,6 @@
 // Disclaimer: lots of object detection code is from https://editor.p5js.org/tomas.decamino/sketches/qRTOcPD5N
 
 let video;
-let detector;
-let surfaces = [];
-
-function preload() {
-  detector = ml5.objectDetector('cocossd'); 
-}
-
-function addSurfaces(err, results) {
-  if (err){
-    console.error(err);
-  }
-
-  surfaces = results;
-  detector.detect(video, addSurfaces);
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -37,22 +22,10 @@ function setup() {
   };
   video = createCapture(constraints);
   video.hide();
-  detector.detect(video, addSurfaces);
 }
 
 function draw() {
   background(220);
 
   image(video,0,0);
-
-  for (const surface of surfaces) {
-    stroke(0, 255, 0);
-    strokeWeight(1);
-    noFill();
-    rect(surface.x, surface.y, surface.width, surface.height);
-    noStroke();
-    fill(255);
-    textSize(24);
-    text(object.label, object.x + 10, object.y + 24);
-  }
 }
