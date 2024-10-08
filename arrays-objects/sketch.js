834e528ab8c61417878c860cf13d6f968ec3a8d0
NY <67083864+nycan@users.noreply.github.com>
2024-10-08 13:47:09 -0600

    add template object recognition

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -5,24 +5,54 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Disclaimer: lots of object detection code is from https://editor.p5js.org/tomas.decamino/sketches/qRTOcPD5N
+
 let video;
 let detector;
-let detections = [];
+let surfaces = [];
 
 function preload() {
   detector = ml5.objectDetector('cocossd'); 
 }
 
+function addSurfaces(err, results) {
+  if (err){
+    console.error(err);
+  }
+
+  surfaces = results;
+  detector.detect(video, addSurfaces);
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  video = createCapture(VIDEO);
-  video.size(windowWidth, windowHeight);
+  const constraints = {
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment"
+      }
+    }
+  };
+  video = createCapture(constraints);
   video.hide();
+  detector.detect(video, addSurfaces);
 }
 
 function draw() {
   background(220);
 
   image(video,0,0);
+
+  for (const surface of surfaces) {
+    stroke(0, 255, 0);
+    strokeWeight(1);
+    noFill();
+    rect(surface.x, surface.y, surface.width, surface.height);
+    noStroke();
+    fill(255);
+    textSize(24);
+    text(object.label, object.x + 10, object.y + 24);
+  }
 }
