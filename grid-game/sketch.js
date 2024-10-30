dd7bb10e03fb15504367645a63f5c476421c7322
NY <67083864+nycan@users.noreply.github.com>
2024-10-30 14:15:51 -0600

    Added rotation controls

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -5,11 +5,14 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let shared;
+//let shared;
 let playerID;
 let cam;
 
-const speed = 1;
+let me;
+let guests;
+
+const speed = 1.5;
 
 function preload() {
   partyConnect(
@@ -17,7 +20,10 @@ function preload() {
     "nycan_server"
   );
 
-  shared = partyLoadShared("players", shared);
+  //shared = partyLoadShared("shared", shared);
+  // position and camera rotation
+  me = partyLoadMyShared({x: 0, y: 0, z: 0, ax: 0, az: 0});
+  guests = partyLoadGuestShareds();
 }
 
 function setup() {
@@ -25,50 +31,46 @@ function setup() {
 
   cam = createCamera();
   setCamera(cam);
-
-  if (shared.players === undefined) {
-    shared.players = [];
-  }
-
-  playerID = shared.players.length;
-  shared.players.push({
-    x: 0,
-    y: 0,
-    z: 0
-  });
 }
 
 function handleKeys() {
-  const dx = [1,0,-1,0];
-  const dz = [0,1,0,-1];
-  const keys = [68,87,65,83]; // dwas
+  const angles = [0, PI/2, -PI/2, PI];
+  const keys = [87,65,83,68]; // wasd
 
   for (let i = 0; i<4; ++i) {
     if (keyIsDown(keys[i])) {
-      shared.players[playerID].x += dx[i]*speed;
-      shared.players[playerID].z += dz[i]*speed;
+      me.x += speed*sin(-me.az+angles[i]);
+      me.z += speed*cos(-me.az+angles[i]);
     }
   }
+}
+
+function calculateRot() {
+  let xRot = mouseY/height * PI - PI/2;
+  cam.tilt(xRot-me.ax);
+  me.ax = xRot;
+
+  let zRot = - (mouseX/width * 2*PI - PI);
+  cam.pan(zRot-me.az);
+  me.az = zRot;
 }
 
 function draw() {
   background(220);
 
   handleKeys();
+  cam.setPosition(me.x,me.y,me.z);
 
-  let pos = shared.players[playerID];
-  cam.setPosition(pos.x,pos.y,pos.z);
+  calculateRot();
 
-  for(let i = 0; i<shared.players.length; ++i) {
-    if (i===playerID) {
-      continue;
-    }
+  push();
+  plane(100,100);
+  pop();
 
-    let player = shared.players[i];
-
+  for (const player of guests) {
     push();
     translate(player.x, player.y, player.z);
-    box(5,10,5);
+    sphere(10);
     pop();
   }
 }
