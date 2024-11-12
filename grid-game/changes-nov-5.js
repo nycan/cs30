e0b050c2d02f67c8c240e3c6b125f6abad83956d
NY <67083864+nycan@users.noreply.github.com>
2024-11-12 14:16:26 -0600

    start working on pvp

--- grid-game/changes-nov-5.js
+++ grid-game/changes-nov-5.js
@@ -1,37 +1,26 @@
-const maxXP = 100;
-const maxXPrad = 15;
-const minXPrad = 3;
 
-xp = Array(xCells).fill().map(
-  (x) => Array(zCells).fill(0)
-);
-setInterval(spawnXP, 20);
-
-function spawnXP() {
-  let randX = floor(random(xCells));
-  let randZ = floor(random(zCells));
-
-  xp[randX][randZ] = min(xp[randX][randZ]+1, maxXP);
+function draw2d() {
+  rect(0,0,10,10);
 }
 
-function drawXP() {
-  emissiveMaterial(34, 240, 54);
-  for(let i = 0; i<xCells; ++i) {
-    for(let j = 0; j<zCells; ++j) {
-      if (!xp[i][j]) {
-        continue;
-      }
-      let rad = lerp(1,maxXPrad, xp[i][j]/maxXP);
+/*
+function linePlaneCollide(guest) {
 
-      push();
-      translate(
-        cellSize*(-xCells/2 + i + 1/2),
-        wallHeight/2-rad,
-        cellSize*(-zCells/2 + j + 1/2)
-      );
-      sphere(rad);
-      pop();
+}
+
+function mousePressed() {
+  if (mouseButton === LEFT) {
+    return;
+  }
+  for (const guest of guests) {
+    if (guest === me) {
+      continue;
+    }
+    // immediately filter out people we can't hit
+    let dst = dist(guest.x,guest.y,guest.z,me.x,me.y,me.z);
+    if (dst > Math.hypot(playerHead/2+playerBody,sqrt(2)*playerWidth)+reach) {
+      continue;
     }
   }
-  emissiveMaterial(0);
 }
+  */
